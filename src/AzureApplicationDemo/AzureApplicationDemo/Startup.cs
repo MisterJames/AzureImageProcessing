38736d1d102e0101648eede6fc5c53aeb331bfca
James Chambers <james@jameschambers.com>
2015-10-12 21:06:15 -0500

    Initial Project Create

---
+++ src/AzureApplicationDemo/AzureApplicationDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureApplicationDemo.Startup))]
+namespace AzureApplicationDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
