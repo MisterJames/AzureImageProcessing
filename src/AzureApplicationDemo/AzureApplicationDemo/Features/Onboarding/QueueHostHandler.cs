dfb8e464d20229ef420619ac6f01b629296ca33e
James Chambers <james@jameschambers.com>
2015-10-24 07:42:49 -0500

    preliminary work to write to queue
    - refactored config
    - added features for new hosts

---
+++ src/AzureApplicationDemo/AzureApplicationDemo/Features/Onboarding/QueueHostHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AzureApplicationDemo.Features.Common;
+using AzureApplicationDemo.Services;
+using MediatR;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Newtonsoft.Json;
+
+namespace AzureApplicationDemo.Features.Onboarding
+{
+    public class QueueHostHandler : RequestHandler<QueueHostCommand>
+    {
+
+        protected override void HandleCore(QueueHostCommand message)
+        {
+            var storageAccount = CloudStorageAccount.Parse(ConfigurationService.ConfigurationValue(ConfigurationService.AzureStorageConnectionString));
+            var client = storageAccount.CreateCloudQueueClient();
+            var queue = client.GetQueueReference("creatednshost");
+            queue.CreateIfNotExists();
+
+            var hostInfo = new HostInformation
+            {
+                HostName = message.Host.HostName,
+                UserId = message.Host.UserId
+            };
+
+            var entry = new CloudQueueMessage(JsonConvert.SerializeObject(hostInfo));
+            queue.AddMessage(entry);
+        }
+    }
+}
---
+++ src/AzureApplicationDemo/AzureApplicationDemo/Services/ConfigurationService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AzureApplicationDemo.Services
+{
+    public static class ConfigurationService
+    {
+        public const string AzureStorageConnectionString = "mva-azure-storage-connectionstring";
+        public const string TwitterKey = "mva-twitter-key";
+        public const string TwitterSecret = "mva-twitter-secret";
+
+        private static readonly Dictionary<string, string> ConfigurationValues = new Dictionary<string, string>();
+
+        private static readonly IEnumerable<string> KnownConfigKeys = new List<string>
+        {
+            AzureStorageConnectionString,
+            TwitterKey,
+            TwitterSecret
+        };
+
+        static ConfigurationService()
+        {
+            foreach (var key in KnownConfigKeys)
+            {
+                ConfigurationValues.Add(key, Environment.GetEnvironmentVariable(key));
+            }
+        }
+
+        public static string ConfigurationValue(string key)
+        {
+            return ConfigurationValues[key];
+        }
+    }
+}
