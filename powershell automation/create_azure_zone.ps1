5b71a19052ccec2c8c55ad1667a425052eb3efd2
James Chambers <james@jameschambers.com>
2015-10-28 08:23:45 -0500

    initial commit for DNS config
    - screenshots
    - notes/links
    - powershell scripts
    
    completes up to but not including A records (recordsets created)

---
+++ powershell automation/create_azure_zone.ps1
@@ -0,0 +1,11 @@
+﻿
+Switch-AzureMode -Name AzureResourceManager
+Add-AzureAccount
+Select-AzureSubscription -SubscriptionName "Free Trial"
+
+New-AzureResourceGroup -Name AzureImageProcessing -location "West US"
+Register-AzureProvider -ProviderNamespace Microsoft.Network
+
+
+New-AzureDnsZone -Name imagenomnom.com -ResourceGroupName AzureImageProcessing
+Get-AzureDnsRecordSet -ZoneName imagenomnom.com -ResourceGroupName AzureImageProcessing
---
+++ powershell automation/create_first_host.ps1
@@ -0,0 +1,8 @@
+﻿
+# get the zone, two required params
+$rootzone = Get-AzureDnsZone -Name imagenomnom.com -ResourceGroupName AzureImageProcessing
+
+# create the subdomain
+$rs = New-AzureDnsRecordSet -Name www -Zone $rootzone -RecordType A -Ttl 300
+
+
