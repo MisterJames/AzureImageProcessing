64c736b8aeb3808660183e10103354c9b4356a99
James Chambers <james@jameschambers.com>
2015-11-08 21:45:39 -0800

    integrating web job code

---
+++ src/AzureApplicationDemo/HostQueueProcessor/Functions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace HostQueueProcessor
+{
+    public class Functions
+    {
+        public static void ProcessQueueMessage([QueueTrigger("creatednshost")] string message, TextWriter log)
+        {
+
+            log.WriteLine(message);
+        }
+    }
+}
